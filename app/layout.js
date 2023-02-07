09d66e8d78bf4c1a57771fc1d2fe219fc983a914
Hari om Singh <www.hariom03.com@gmail.com>
2023-02-08 02:03:53 +0530

    nav done

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,7 @@
-import './globals.css'
+"use client";
+import Navbar from "@/components/navbar/Navbar";
+import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
 export default function RootLayout({ children }) {
   return (
@@ -8,7 +11,12 @@ export default function RootLayout({ children }) {
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <ThemeProvider enableSystem={true} attribute="class">
+          <Navbar />
+        </ThemeProvider>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
---
+++ components/navbar/Navbar.jsx
@@ -0,0 +1,90 @@
+"use client";
+import React, { useState } from "react";
+import { Link } from "react-scroll/modules";
+import { useTheme } from "next-themes";
+import { RiMoonFill, RiSunLine } from "react-icons/ri";
+import { IoMdMenu, IoMdClose } from "react-icons/io";
+
+const Navbar = () => {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === "system" ? systemTheme : theme;
+  const [navbar, setNavbar] = useState(false);
+  return (
+    <header className="w-full mx-auto px-4  shadow fixed top-0 z-50 dark:border-stone-600">
+      <div className="justify-between md:items-center md:flex">
+        <div>
+          <div className="flex items-center justify-between">
+            <div className="md:py-3 md:block">
+              <h2 className="text2xl font-bold">Harixomxsingh</h2>
+            </div>
+            <div className="md:hidden">
+              <button onClick={() => setNavbar(!navbar)}>
+                {navbar ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
+              </button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div
+            className={`flex-1 justify-self-center md:flex md:pb-0 md:mt-0 ${
+              navbar ? "block" : "hidden"
+            }`}
+          >
+            <ul className="md:flex md:flex-row md:items-center sm:px-20 flex-col md:space-x-6 space-y-8 md:space-y-0">
+              <Link
+                to="/"
+                className="block lg:inline-block text-neutral-900 hover:text-neutral-500 dark:text-neutral-500 cursor-pointer"
+                activeClass="active"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={500}
+              >
+                Home
+              </Link>
+              <Link
+                to="/about"
+                className="block lg:inline-block text-neutral-900 hover:text-neutral-500 dark:text-neutral-300 cursor-pointer"
+                activeClass="active"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={500}
+              >
+                About
+              </Link>
+              <Link
+                to="/project"
+                className="block lg:inline-block text-neutral-900 hover:text-neutral-500 dark:text-neutral-300 cursor-pointer"
+                activeClass="active"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={500}
+              >
+                Projects
+              </Link>
+              {currentTheme === "dark" ? (
+                <button
+                  className="bg-slate-100 p-2 rounded-xl"
+                  onClick={() => setTheme("light")}
+                >
+                  <RiSunLine size={25} color="black" />
+                </button>
+              ) : (
+                <button
+                  className="bg-slate-100 p-2 rounded-xl"
+                  onClick={() => setTheme("dark")}
+                >
+                  <RiMoonFill size={25} />
+                </button>
+              )}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
