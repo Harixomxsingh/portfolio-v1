fe106d7e5cf492d6696580b62080ec1b07aca805
Hari om Singh <www.hariom03.com@gmail.com>
2023-02-09 00:08:08 +0530

    made the Herosection homesection but nav have some bug on it and arrow not animated

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "./page.module.css";
+import HeroSection from "@/components/heroSection/HeroSection";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main id="/">
-      <h1>hello world</h1>
+    <main id="/" className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
+      <HeroSection />
     </main>
   );
 }
